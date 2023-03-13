ff7dedcc621977e4310fe0cac10e33f379d4af4a
unknown <g00406367@atu.ie>
2023-03-13 10:48:26 +0000

    Lab6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'student_app';
+export class AppComponent implements OnInit {
+  myStudents:any[]=[];
+  myWeather: any[]=[];
+  location:string="";
+  constructor(private service:DataService){
+
+  }
+
+  ngOnInit(): void {
+    this.service.GetStudentData().subscribe(
+      (data)=>{
+        this.myStudents= data.students;
+      }
+
+    );
+    this.service.GetWeatherData().subscribe(
+      (data)=>{
+        this.myWeather = data.weather;
+        this.location = data.name;
+      }
+    )
+ 
+
+  }
 }
+
